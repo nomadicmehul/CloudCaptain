d4340f3fba0d2cf51106496682caad354750d0e6
Mehul Patel <11514627+nomadicmehul@users.noreply.github.com>
2026-03-21 18:14:56 +0100

    Add: GitHub star count navbar button, replace GitHub/Twitter links

---
+++ website/src/theme/NavbarItem/ComponentTypes.tsx
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import GitHubStarsNavbarItem from './GitHubStarsNavbarItem';
+
+export default {
+  ...ComponentTypes,
+  'custom-githubStars': GitHubStarsNavbarItem,
+};
